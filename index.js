52229cfcaf4ed5541c29abd91c462ce54f858264
bthesorceror <bthesorceror@gmail.com>
2013-07-15 23:34:14 -0400

    Allow duplex emitters for clients
    * allows linked servers to share events both ways

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function PublicRadio(port) {
   this.server  = this.createServer();
   this.port    = port;
   this.emitter = new EventEmitter();
+  this.connections = [];
 }
 
 util.inherits(PublicRadio, EventEmitter);
@@ -24,28 +25,55 @@ PublicRadio.prototype.createServer = function() {
 
 PublicRadio.prototype.handler = function(socket) {
   var self = this;
-  emitstream(this.emitter).pipe(json.stringify()).pipe(socket);
-  this.emit('connection', socket);
+  var connection = new ClientConnection(socket);
+  this.emit('connection', connection);
 
   var disconnect = proxy(function() {
     this.emit('disconnect', socket);
+    this.removeConnection(connection);
   }, this)
 
   var error = proxy(function(err) {
     this.emit('error', err);
+    disconnect();
   }, this)
+
+  this.connections.push(connection);
+
+  connection.events.on('incoming', function() {
+    self._broadcast(arguments, connection);
+  });
 
   socket.on('close', disconnect)
   socket.on('end', disconnect)
   socket.on('error', error);
 }
 
+PublicRadio.prototype._broadcast = function(args, exclude) {
+  this.connections.forEach(function(conn) {
+    if (conn != exclude) {
+      conn.emit.apply(conn, args);
+    }
+  });
+}
+
 PublicRadio.prototype.broadcast = function() {
-  this.emitter.emit.apply(this.emitter, arguments);
+  this._broadcast(arguments);
 }
 
 PublicRadio.prototype.listen = function() {
   this.server.listen(this.port);
+}
+
+PublicRadio.prototype.addConnection = function(connection) {
+  this.connections.push(connection);
+}
+
+PublicRadio.prototype.removeConnection = function(connection) {
+  var index = this.connections.indexOf(connection);
+  if (index >= 0) {
+    this.connections.splice(index);
+  }
 }
 
 PublicRadio.prototype.linkTo = function(host, port) {
@@ -54,7 +82,13 @@ PublicRadio.prototype.linkTo = function(host, port) {
 
   client.on('connected', function(connection) {
     connection.events.on('incoming', function() {
-      self.emitter.emit.apply(self.emitter, arguments);
+      self._broadcast(arguments, connection);
+    });
+
+    self.addConnection(connection);
+
+    client.on('disconnected', function(conn) {
+      self.removeConnection(connection);
     });
   });
 
@@ -96,7 +130,7 @@ function PublicRadioClient(host, port) {
 util.inherits(PublicRadioClient, EventEmitter);
 
 PublicRadioClient.prototype.disconnected = function() {
-  this.emit('disconnected', this.emitter);
+  this.emit('disconnected', this.connection);
 }
 
 PublicRadioClient.prototype.error = function(err) {
